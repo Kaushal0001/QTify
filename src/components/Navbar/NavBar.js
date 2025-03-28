6014a0d237ef2737e6e6bc574d2572c62036f7f9
Kaushal <86143686+Kaushal0001@users.noreply.github.com>
2025-03-28 09:34:01 +0000

    final

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,32 +2,37 @@ import Logo from "../Logo/Logo";
 import styles from "./NavBar.module.css";
 import Button from "../Button/Button";
 import SearchBar from "../SearchBar/SearchBar";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 
-// const NavBar = ({ data, setFeedbackFlag }) => {
-//   return (
-//     <nav className={styles.navbar}>
-//       <Logo />
-//       <SearchBar data={data} placeholder="Search a song of your choice" />
-//       <Button text="Give Feedback" setFeedbackFlag={setFeedbackFlag} />
-//     </nav>
-//   );
-// };
-
-function NavBar({ searchData,setFeedbackFlag }) {
+const NavBar = ({ data, setFeedbackFlag }) => {
   return (
     <nav className={styles.navbar}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <SearchBar
-        placeholder="Search a song of your choice"
-        searchData={searchData}
-      />
-       <Button text="Give Feedback" setFeedbackFlag={setFeedbackFlag} />
+      <Logo />
+      <SearchBar data={data} placeholder="Search a song of your choice" />
+      <Button text="Give Feedback" setFeedbackFlag={setFeedbackFlag} />
     </nav>
   );
-}
+};
+
+
+
 
 export default NavBar;
+
+
+
+// function NavBar({ searchData,setFeedbackFlag }) {
+//   return (
+//     <nav className={styles.navbar}>
+//       <Link to="/">
+//         <Logo />
+//       </Link>
+//       <SearchBar
+//         placeholder="Search a song of your choice"
+//         searchData={searchData}
+//       />
+//        <Button text="Give Feedback" setFeedbackFlag={setFeedbackFlag} />
+//     </nav>
+//   );
+// }
